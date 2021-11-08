9834eedac0e3ad7ffc01e0ae7338738a2e3b1f59
soonteckling <soonteckl@outlook.com>
2021-11-08 20:59:04 +0800

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'greeting', to: 'questions#hello'
+
+  get 'ask', to: 'questions#ask'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'answer', to: 'questions#answer'
 end
